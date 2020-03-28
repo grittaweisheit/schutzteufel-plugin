339e8b8bef745d9eb41072ca2237543cc3915f0c
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-03-28 19:40:46 +0100

    add profile blocktype from tutorial

---
+++ block-types/single-recent-post/index.php
@@ -0,0 +1,38 @@
+<?php
+function schutzteufel_single_recent_post_block() {
+
+    // Scripts.
+    wp_register_script(
+        'schutzteufel-single-recent-post-block-script', // Handle.
+        plugins_url( 'block.js', __FILE__ ), // Block.js: We register the block here.
+        array( 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-editor' ), // Dependencies, defined above.
+        filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ),
+        true // Load script in footer.
+    );
+
+    // Styles.
+    wp_register_style(
+        'schutzteufel-single-recent-post-block-editor-style', // Handle.
+        plugins_url( 'editor.css', __FILE__ ), // Block editor CSS.
+        array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+        filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
+    );
+    wp_register_style(
+        'schutzteufel-single-recent-post-block-frontend-style', // Handle.
+        plugins_url( 'style.css', __FILE__ ), // Block editor CSS.
+        array(), // Dependency to include the CSS after it.
+        filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+    );
+
+    } // End function organic_profile_block().
+
+	// Register the block with WP using our namespacing
+	// We also specify the scripts and styles to be used in the Gutenberg interface
+	register_block_type( 'single-recent-post/block', array(
+		'editor_script' => 'schutzteufel-single-recent-post-block-script',
+		'editor_style' => 'schutzteufel-single-recent-post-block-editor-style',
+		'style' => 'schutzteufel-single-recent-post-block-frontend-style',
+    ) );
+    
+    add_action( 'init', 'schutzteufel_single_recent_post_block' );
+?>
--- schutzteufel-plugin.php
+++ schutzteufel-plugin.php
@@ -1,10 +1,28 @@
 <?php
-/*
-Plugin Name: Site Plugin for schutzteufel.de
-Description: Site specific code changes for schutzteufel.de
-*/
-/* Start Adding Functions Below this Line */
+/**
+ * Plugin Name: Site Plugin for schutzteufel.de
+ * Description: Site specific code changes for schutzteufel.de
+ * @package ST
+ */
 
+if ( ! defined( 'ST_VERSION' ) ) {
+	define( 'ST_VERSION', '1.4.1' );
+}
 
-/* Stop Adding Functions Below this Line */
+if ( ! defined( 'ST_NAME' ) ) {
+	define( 'ST_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
+}
+
+if ( ! defined( 'ST_DIR' ) ) {
+	define( 'ST_DIR', WP_PLUGIN_DIR . '/' . ST_NAME );
+}
+
+if ( ! defined( 'ST_URL' ) ) {
+	define( 'ST_URL', WP_PLUGIN_URL . '/' . ST_NAME );
+}
+/**
+ * BLOCK: Profile Block.
+ */
+require_once ST_DIR . '/block-types/profile/index.php';
+
 ?>
