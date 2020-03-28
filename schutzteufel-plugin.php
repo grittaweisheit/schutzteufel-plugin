fba274003814c4536f50a31503c6733f70ea9337
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-03-28 17:54:42 +0100

    add su-posts template for single recent post

---
+++ schutzteufel-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Site Plugin for schutzteufel.de
+Description: Site specific code changes for schutzteufel.de
+*/
+/* Start Adding Functions Below this Line */
+
+
+/* Stop Adding Functions Below this Line */
+?>
