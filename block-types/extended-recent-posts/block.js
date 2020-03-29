c11cddff28d47b7c47d66cf42ba59ca8e2e9f4e4
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-03-29 21:52:38 +0200

    add functioning prototype for extended recent post block

--- block-types/extended-recent-posts/block.js
+++ block-types/extended-recent-posts/block.js
@@ -1,114 +1,24 @@
-(function (blocks, editor, components, i18n, element, data ) {
-    var __ = i18n.__
-    var el = element.createElement
-    var registerBlockType = blocks.registerBlockType
-    var withSelect = data.withSelect
-    var RichText = editor.RichText
-    var BlockControls = editor.BlockControls
-    var AlignmentToolbar = editor.AlignmentToolbar
-    var InspectorControls = editor.InspectorControls
-    var TextControl = components.TextControl
-
-    registerBlockType('schutzteufel/extended-recent-posts-block', { // The name of our block. Must be a string with prefix. Example: my-plugin/my-custom-block.
-        title: __('Recent Posts'), // The title of our block.
-        description: __('A custom block for displaying recent posts.'), // The description of our block.
-        icon: 'megaphone', // Dashicon icon for our block. Custom icons can be added using inline SVGs.
-        category: 'widgets', // The category of the block.
-        supports: {
+( function( blocks, editor, components, i18n, element, data ) {
+    var __ = i18n.__;
+	var el = element.createElement,
+        registerBlockType = blocks.registerBlockType,
+        withSelect = data.withSelect,
+    	RichText = editor.RichText,
+     	BlockControls = editor.BlockControls,
+     	AlignmentToolbar = editor.AlignmentToolbar,
+     	InspectorControls = editor.InspectorControls,
+     	TextControl = components.TextControl;
+ 
+    registerBlockType( 'schutzteufel/extended-recent-posts-block', {
+        title: 'Recent Posts',
+        icon: 'businessman',
+        category: 'widgets',
+		 supports: {
             align: true,
             alignWide: true
         },
-        attributes: { // Necessary for saving block content.
-            align: {
-                type: 'string',
-                default: 'left'
-            },
-            className: {
-                type: 'string',
-            },
-            categories: {
-                type: 'string',
-            },
-            columns: {
-                type: 'number',
-                default: 1,
-            },
-            displayFeaturedImage: {
-                type: 'boolean',
-                default: false,
-            },
-            displayPostContent: {
-                type: 'boolean',
-                default: true,
-            },
-            displayPostContentRadio: {
-                type: 'string',
-                default: 'excerpt',
-            },
-            displayPostDate: {
-                type: 'boolean',
-                default: false,
-            },
-            excerptLength: {
-                type: 'number',
-                default: 55,
-            },
-            featuredImageAlign: {
-                type: 'string',
-                enum: array( 'left', 'center', 'right' ),
-            },
-            featuredImageSizeSlug: {
-                type: 'string',
-                default: 'thumbnail',
-            },
-            featuredImageSizeWidth: {
-                type: 'number',
-                default: null,
-            },
-            featuredImageSizeHeight: {
-                type: 'number',
-                default: null,
-            },
-            numberOfPosts: {
-                type: 'number',
-            },
-            order: {
-                type: 'string',
-                default: 'desc',
-            },
-            orderBy: {
-                type: 'string',
-                default: 'date',
-            },
-            postLayout: {
-                type: 'string',
-                default: 'list',
-            },
-            smallTitleWithLink: { // display title in normal size as link
-                type: 'boolean',
-                default: true,
-            },
-            titleTag: { // tag applied to title
-                type: 'string',
-                default: 'h1',
-            },
-            titleWithLink: { // title is link to post
-                type: 'boolean',
-                default: 'false'
-            },
-
-            content1: {
-                type: 'string',
-                default: 'Hello World'
-            },
-            content2: {
-                type: 'string',
-                default: 'Hello World 2'
-            }
-        },
-
-        // this is what you see and what happens while editing your website/page/post
-        edit: 
+		
+		edit:
             withSelect( function( select ) {
                 return {
                     posts: select( 'core' ).getEntityRecords( 'postType', 'post' ),
@@ -166,134 +76,17 @@
                         el('a', {href: posts[0].link}, posts[0].title.rendered)
                     )
                 ]
-            },)
-/* 
-        // this is what will actually appear on your website
-        save: function (props) {
-            var attributes = props.attributes
-            var alignment = props.attributes.alignment
-
-            return (
-                el('div', {style: { textAlign: alignment }},
-                    el(RichText.Content, {
-                        className: 'schutzteufel-profile-bio',
-                        tagName: 'h1',
-                        value: attributes.content2
-                    }),
-                    el('p', {}, attributes.content1)
-                )
-            )
-        } */
-    })
-})(
+            },),
+		save: function(props){
+			// here could be a fallback html return value
+			return null
+		}
+    } );
+}(
     window.wp.blocks,
     window.wp.editor,
     window.wp.components,
     window.wp.i18n,
     window.wp.element,
     window.wp.data
-)
-
-/* import { registerBlockType } from '@wordpress/blocks';
-import { 
-    RichText,
-    AlignmentToolbar,
-    BlockControls, 
-} from '@wordpress/block-editor';
-import { withSelect } from '@wordpress/data';
- 
-registerBlockType( 'schutzteufel/single-recent-post', {
-    title: 'Most recent post',
-    icon: 'megaphone',
-    category: 'widget',
-    attributes: {
-        category,
-    },
-
-    edit: withSelect((select) => {
-        return {
-            posts: select('core').getEntityRecords('postType', 'post'),
-        } ;
-    }) (({posts, className}) => {
-        if ( ! posts ) {
-            return 'Loading...';
-        }
- 
-        if ( posts && posts.length === 0 ) {
-            return 'No posts';
-        }
- 
-        const post = posts[ 0 ];
- 
-        return <a className={ className } href={ post.link }>
-            { post.title.rendered }
-        </a>;
-    }),
-    
- */
-
-    /* attributes: {
-        content: {
-            type: 'array',
-            source: 'children',
-            selector: 'p',
-        },
-        alignment: {
-            type: 'string',
-            default: 'none',
-        },
-    },
-    example: {
-        attributes: {
-            content: 'Hello World',            
-            alignment: 'right',
-        },
-    }, 
-
-    edit: ( props ) => {
-        const { 
-            attributes: { 
-                content, 
-                alignment, 
-            }, 
-            setAttributes, 
-            className ,
-        } = props;
-
-        const onChangeContent = ( newContent ) => {
-            props.setAttributes( { content: newContent } );
-        }; 
-        const onChangeAlignment = ( newAlignment ) => {
-            props.setAttributes( { alignment: newAlignment === undefined ? 'none' : newAlignment } );
-        };
-        
-        return (
-            <div>
-                {
-                    <BlockControls>
-                        <AlignmentToolbar
-                            value={ alignment }
-                            onChange={ onChangeAlignment }
-                        />
-                    </BlockControls>
-                }
-                <RichText
-                    className={ className }
-                    style={ { textAlign: alignment } }
-                    tagName="p"
-                    onChange={ onChangeContent }
-                    value={ content }
-                />
-            </div>
-        );
-    },
-
-    save: ( props ) => {
-        return (
-            <RichText.Content
-                className={ `schutzteufel-single-recent-post-${ props.attributes.alignment }` }
-                tagName="p"
-                value={ props.attributes.content }
-            />
-        );    
-    }, */
+) );
--- block-types/extended-recent-posts/index.php
+++ block-types/extended-recent-posts/index.php
@@ -13,40 +13,37 @@ defined( 'ABSPATH' ) || exit;
  * `wp-element`: Includes the WordPress Element abstraction for describing the structure of your blocks.
  * `wp-i18n`: To internationalize the block's text.
  * `wp-data`: To access posts and other data.
+ * `wp-editor`: dont know
  *
  * @since 1.0.0
  */
 
 function schutzteufel_extended_recent_posts_block_render_callback($attributes, $content){
-	$recent_posts = wp_get_recent_posts(array(
-		'numberposts' => $attributes.numberOfPosts,
-		'post_status' => 'publish',
-	));
-	
-	if ( count( $recent_posts ) === 0 ) {
+    $recent_posts = wp_get_recent_posts( array(
+        'numberposts' => 1,
+        'post_status' => 'publish',
+    ) );
+    if ( count( $recent_posts ) === 0 ) {
         return 'No posts';
     }
     $post = $recent_posts[ 0 ];
     $post_id = $post['ID'];
     return sprintf(
-        '<a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a>',
+        '<h1>%3$s</h1> <a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a>',
         esc_url( get_permalink( $post_id ) ),
-        esc_html( get_the_title( $post_id ) )
+        esc_html( get_the_title( $post_id ) ),
+		$attributes['displayPostContentRadio']
     );
 }
 
 function schutzteufel_extended_recent_posts_block() {
 
-	if ( ! function_exists( 'register_block_type' ) ) {
-		// Gutenberg is not active.
-		return;
-	}
-
 	// Scripts.
+	// enqueue so the block gets registered
 	wp_enqueue_script(
-		'schutzteufel-extended-recent-posts-block-script', // Handle.
+		'schutzteufel-extended-recent-posts-block', // Handle.
 		plugins_url( 'block.js', __FILE__ ), // Block.js: We register the block here.
-		array( 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-editor', 'wp-data' ), // Dependencies, defined above.
+		array( 'wp-blocks', 'wp-editor', 'wp-components', 'wp-i18n', 'wp-element', 'wp-data' ), // Dependencies, defined above.
 		filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ),
 		true // Load script in footer.
 	);
@@ -69,14 +66,14 @@ function schutzteufel_extended_recent_posts_block() {
 		plugins_url( 'font-awesome.css', __FILE__ ), // Font Awesome for social media icons.
 		array(),
 		'4.7.0'
-	);
+	); 
 
-	// Here we actually register the block with WP, again using our namespacing.
+	// Here we actually register the block with WP using our namespacing.
 	// We also specify the editor script to be used in the Gutenberg interface.
 	register_block_type(
-		'extended-recent-posts/block',
-		array(
-			'editor_script' => 'schutzteufel-extended-recent-posts-block-script',
+		'schutzteufel/extended-recent-posts-block',
+		 array(
+            'attributes' => attributes,
 			'editor_style'  => 'schutzteufel-extended-recent-posts-block-editor-style',
 			'render_callback' => 'schutzteufel_extended_recent_posts_block_render_callback',
 			'style'         => 'schutzteufel-extended-recent-posts-block-frontend-style',
@@ -85,5 +82,74 @@ function schutzteufel_extended_recent_posts_block() {
 
 } // End function schutzteufel_extended_recent_posts_block().
 
-// Hook: Editor assets.
 add_action( 'init', 'schutzteufel_extended_recent_posts_block' );
+
+
+const attributes = array(
+	'align' => array(
+		'type' => 'string',
+		'enum' => array( 'left', 'center', 'right', 'wide', 'full' ),
+	),
+	'categories' => array(
+		'type' => 'string',
+	),
+	'className' => array(
+		'type' => 'string',
+	),
+	'columns' => array(
+		'type'    => 'number',
+		'default' => 3,
+	),
+	'displayFeaturedImage' => array(
+		'type'    => 'boolean',
+		'default' => false,
+	),
+	'displayPostContent' => array(
+		'type'    => 'boolean',
+		'default' => false,
+	),
+	'displayPostContentRadio' => array(
+		'type'    => 'string',
+		'default' => 'excerpt',
+	),
+	'displayPostDate'         => array(
+		'type'    => 'boolean',
+		'default' => false,
+	),
+	'excerptLength' => array(
+		'type'    => 'number',
+		'default' => 55,
+	),
+	'featuredImageAlign'      => array(
+		'type' => 'string',
+		'enum' => array( 'left', 'center', 'right' ),
+	),
+	'featuredImageSizeHeight' => array(
+		'type'    => 'number',
+		'default' => null,
+	),
+	'featuredImageSizeSlug'   => array(
+		'type'    => 'string',
+		'default' => 'thumbnail',
+	),
+	'featuredImageSizeWidth'  => array(
+		'type'    => 'number',
+		'default' => null,
+	),
+	'numberOfPosts' => array(
+		'type'    => 'number',
+		'default' => 5,
+	),
+	'order' => array(
+		'type'    => 'string',
+		'default' => 'desc',
+	),
+	'orderBy'                 => array(
+		'type'    => 'string',
+		'default' => 'date',
+	),
+	'postLayout'              => array(
+		'type'    => 'string',
+		'default' => 'list',
+	)
+);
